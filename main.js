5f9cfe393ea6cd186b97932718441ca45374f926
Davi2B252 <davi.souza.soares@escola.pr.gov.br>
2025-11-23 22:39:02 -0300

    .

--- main.js
+++ main.js
@@ -8,10 +8,62 @@ let contagemPalpites = 1;
 let botaoReiniciar;
 
 function verificarpalpites() {
-    const palpiteusuario = number(campoPalpite.value);
+    const palpiteUsuario = Number(campoPalpite.value);
     if (contagemPalpites === 1) {
-        palpites.textContent = 'palpites anteriores'
-
+        palpites.textContent = 'palpites anteriores';
     }
 }
-    
+
+palpites.textContent += palpiteUsuario + " ";
+
+if (palpiteUsuario === numeroAleatorio) {
+  ultimoResultado.textContent = "Parabéns! Você Acertou!";
+  ultimoResultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else if (contagemPalpites === 10) {
+  ultimoResultado.textContent = "FIM DE JOGO!!";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else {
+  ultimoResultado.textContent = "Errado";
+  ultimoResultado.style.backgroundColor = "red";
+  if (palpiteUsuario < numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito baixo";
+  } else if (palpiteUsuario > numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito alto";
+  }
+}
+
+contagemPalpites++;
+campoPalpite.value = "";
+campoPalpite.focus();
+
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+  campoPalpite.disabled = true;
+  envioPalpite.disabled = true;
+  botaoReiniciar = document.createElement('button');
+  document.body.appendChild(botaoReiniciar);
+  botaoReiniciar.textContent = 'Reiniciar Jogo';
+  botaoReiniciar.classList.add('botaoReiniciar');
+  botaoReiniciar.addEventListener('click', reiniciarJogo);
+}
+
+function reiniciarJogo() {
+  contagemPalpites = 1;
+  const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultados p');
+  for (const paragrafoReiniciar of paragrafosReiniciar) {
+    paragrafoReiniciar.textContent = "";
+}
+
+  botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+  campoPalpite.disabled = false;
+  envioPalpite.disabled = false;
+  campoPalpite.value = "";
+  campoPalpite.focus();
+  ultimoResultado.style.backgroundColor = 'white';
+  numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+} 
